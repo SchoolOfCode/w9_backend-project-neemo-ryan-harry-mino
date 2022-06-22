3cc5b56761fc336e4a3c6659243bd32ea6f7d10f
Neemo Dab <neemodab@gmail.com>
2022-06-22 12:11:42 +0100

    cors fixed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // imports
 import express from "express";
 import quotesRouter from "./db/routes/quotesRouter.js";
+import cors from 'cors'
 
 // creating App using express
 const app = express();
@@ -14,13 +15,18 @@ app.get("/", function (req, res) {
     message: "success",
   });
 });
-
+// 
+// app.get('/products/:id', function (req, res, next) {
+  // res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
+// 
 // App is listening on port
 app.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}`);
 });
 
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/quotes", quotesRouter);
